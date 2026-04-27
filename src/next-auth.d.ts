5aa9b5d3e0a893b472336171d265ce232075f884
mario12185 <rhemacorporation.office@gmail.com>
2026-04-27 19:59:45 +0000

    fix: extend next-auth types for phone & role

---
+++ src/next-auth.d.ts
@@ -0,0 +1,22 @@
+﻿import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role?: string;
+      phone?: string;
+    } & DefaultSession["user"];
+  }
+  interface User extends DefaultUser {
+    role?: string;
+    phone?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    role?: string;
+    phone?: string;
+  }
+}
